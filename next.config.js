3e21545d7ca80cb30a7555c330824921a5253116
elluis <elluis.codes@gmail.com>
2023-02-27 08:04:43 -0600

    sanitize.css installed and configured -
    npm install sanitize.css --save
    	installation method(s)
    		installation
    			pnpm install sanitize.css --save
    			https://elluiscodes.jetbrains.space/p/main/issues/13
    	import || use method(s)
    		import via CSS
    			@import '~sanitize.css';
    			@import '~sanitize.css/forms.css';
    			@import '~sanitize.css/typography.css';
    		import via JS
    			import 'sanitize.css';
    			import 'sanitize.css/forms.css';
    			import 'sanitize.css/typography.css';
    	https://github.com/csstools/sanitize.css

--- next.config.js
+++ next.config.js
@@ -1,8 +1,31 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   experimental: {
-    appDir: true,
+    appDir: true
   },
-}
-
-module.exports = nextConfig
+  webpack: (config, options) => {
+    config.module.rules.push({
+                               test: /\.(mp4)$/i,
+                               use: [
+                                 {
+                                   loader: 'url-loader',
+                                   options: {
+                                     limit: 8192,
+                                     fallback: require.resolve('file-loader'),
+                                     publicPath: `/_next/static/videos/`,
+                                     outputPath: `${options.isServer ? '../' : ''}static/videos/`,
+                                     name: '[name].[hash].[ext]'
+                                   }
+                                 }
+                               ]
+                             });
+    return config;
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+};
