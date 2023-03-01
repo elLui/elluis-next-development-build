9941a62db97ec05ca4f2dd1da8210db8334d4ed3
elluis <elluis.codes@gmail.com>
2023-02-28 23:51:23 -0600

    making a quick save - ahem commit! here - as we delve into the realm of https://mui.com/material-ui/react-grid/
    Hopefully it seems not as intrusive and overtaking as a lot of the other solutions out there
    
    Going with this because I want my positioning to be pretty much agnostic from styles and other logic -
    
    There are 2 reasons for this choice, as of this moment:
    
    As it stand control flow is going to be based on time and user interaction - since we have a 24hr schedule -

--- app/page.jsx
+++ app/page.jsx
@@ -1,21 +1,27 @@
+import FeaturedPosts from "@/components/featured-posts/featured-posts";
 import Hero from "@/components/hero/hero";
 import Image from 'next/image'
-import {  Inter } from 'next/font/google'
+import { Inter } from 'next/font/google'
 import { Fragment } from "react";
 import styles from './home.module.scss'
+import Grid from '@mui/material/Grid';
 
 
 
 
 const inter = Inter( { subsets : [ 'latin' ] } )
 
+
+
+
 export default function Home() {
 	return (
 
-		<Fragment>
+		<Grid>
 			<Hero/>
+			<FeaturedPosts/>
 
 
-		</Fragment>
+		</Grid>
 	)
 }
