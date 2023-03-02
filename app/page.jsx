eed2e4610c993e9549b71df5b2130aee5eeafd57
elluis <elluis.codes@gmail.com>
2023-03-02 11:35:04 -0600

    gogogogo ++ CSS STYLING BOOT CAMP PRIVATE!
    THESE STYLES (MY STYLZE - from the dark side - MY STYLEZ ARE - teh CRAHZIEST) ::
    
    !! 1. MUST BE IN A WORKING CONDITION - CHECK ✔
    !! 2. MUST INCLUDE A COMPREHENSIVE DESCRIPTION OF CURRENT GOAL
      - TASK: REFACTOR GLOBAL CSS
        -- REQUIREMENTS: REFACTOR BASE CSS LAYOUT
        * all added experimental global styles are being added to bottom of globals.scss until is confirmed to work
        * then it can be migrated to its appropriate location
    
    !! 2. MUST INCLUDE A DETAILED DESCRIPTION OF CURRENT WORK -

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,9 @@
 "use client"
 import FeaturedPosts from "@/components/featured-posts/featured-posts";
 import Hero from "@/components/hero/hero";
-import Grid2 from "@mui/material/Unstable_Grid2";
-import Image from 'next/image'
+
 import { Inter } from 'next/font/google'
-import { Fragment } from "react";
-import styles from './home.module.scss'
-import Grid from '@mui/material/Grid';
+
 
 
 
