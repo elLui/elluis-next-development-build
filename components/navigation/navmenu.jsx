20ad847c0c9a626ab10a557b4c4e0e7ba879c65c
elluis <elluis.codes@gmail.com>
2023-03-02 22:40:12 -0600

    saving commit - still working on working out a few styling decisions

--- components/navigation/navmenu.jsx
+++ components/navigation/navmenu.jsx
@@ -2,7 +2,6 @@
 // NavMenu.jsx
 import { nav_data } from "@/data/nav-data";
 import NavItem from "@/components/navigation/nav-item/nav-item";
-import Grid2 from '@mui/material/Unstable_Grid2'; // Grid version 2
 import styles from "components/navigation/navmenu.module.scss";
 
 
