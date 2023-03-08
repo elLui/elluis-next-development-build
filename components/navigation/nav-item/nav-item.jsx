13845eb84b58cd295f510c0a49fa2f655c0dc94f
elluis <elluis@localhost.localdomain>
2023-03-08 12:03:51 -0500

    still going strong - we are 'evolving' -
    getting rid of all styling engines - they are stupid handy capped crutches - at least for my purpose at this moment

--- components/navigation/nav-item/nav-item.jsx
+++ components/navigation/nav-item/nav-item.jsx
@@ -1,90 +1,26 @@
-"use client";
-
-import { motion } from "framer-motion";
-import Link from "next/link";
-import styled from "styled-components";
-import { nav_data } from "@/data/nav-data";
+import React from 'react';
+import Link from 'next/link';
+import styled from 'styled-components';
 
 
+const NavItem = ({nav_data}) => {
+    return (
 
 
-const NavItemList = styled.ul`
+        <div>
+            {nav_data.map(({id, label, link, isActive, icon}) => (
+                <li key={id}>
+                    <Link href={link} className="navLink">
+                        {label}
+                        <br/>
+                        <div className="icon_wrapper">{icon}</div>
+                    </Link>
+                </li>
+            ))}
+        </div>
 
 
+    );
+};
 
-
-
-`;
-
-
-export default function NavItem( {} ) {
-	
-	return (
-		
-		
-		<NavItemList>
-			{
-				
-				nav_data.map( ( nav_item ) => {
-					
-					return (
-						
-						<li key={ nav_item.id }>
-							<Link href={ nav_item.link } className="navLink">
-								{ nav_item.label }
-								<br/>
-								<div className="icon_wrapper">
-									{ nav_item.icon }
-								</div>
-							</Link>
-						</li>
-					);
-				} ) }
-		</NavItemList>
-	
-	);
-}
-
-
-
-
-// SUB-ONE-HUNDRED-CLUB
-//"use client"
-//// NavItem.jsx
-//import { motion } from "framer-motion";
-//import Link from "next/link";
-//import clsx from "clsx";
-//import { Grid } from "@mui/material";
-//import styles from "components/navigation/nav-item/nav-item.module.scss";
-//
-//
-//
-//
-//export default function NavItem( { item } ) {
-//
-//	const { id, label, link, isActive, icon } = item;
-//	return (
-//		<Grid item xs={ 12 } sm={ 6 } md={ 4 } lg={ 3 } xl={ 2 }>
-//			<li className={ styles.navItem }>
-//				<Link href={ link } className={ styles.navLink } title={ label }>
-//					<Grid container alignItems="center" justifyContent="center">
-//						<Grid item xs={ 12 }>
-//							{ label }
-//						</Grid>
-//						{ item.icon && (
-//							<Grid item xs={ 12 }>
-//								<motion.div
-//									className={ clsx( styles.navIcon, {
-//										[ styles.animate ] : true,
-//									} ) }
-//								>
-//									{ item.icon }
-//								</motion.div>
-//							</Grid>
-//						) }
-//					</Grid>
-//				</Link>
-//			</li>
-//		</Grid>
-//	);
-//}
+export default NavItem;
